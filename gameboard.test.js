ec8d8bd9d75529a8fff9e55048d0a44025585edb
Shumaila <sayedshumaila27@gmail.com>
2025-01-29 22:03:16 +0530

    Use beforeEach matcher

--- gameboard.test.js
+++ gameboard.test.js
@@ -1,33 +1,28 @@
 import Gameboard from './Gameboard'
 import Ship from './ship'
 
+let game;
+
+beforeEach(() => {
+    game = new Gameboard();
+    game.createBoard();
+})
+
 it('Gameboard class exists', () => {
     expect(typeof Gameboard).toBe('function')
 })
 
-// it('Gameboard creates an array', () => {
-//     const game = new Gameboard()
-//     expect(game.board).toEqual([])
-// })
-
 it('Gameboard is not empty', () => {
-    const game = new Gameboard();
-    game.createBoard()
     expect(game.board.length).toBe(10);
 })
 
-
 it('ship on board', () => {
-    const game = new Gameboard();
-    game.createBoard()
     const ship = new Ship('Submarine', 3);
     game.placeShip(ship, [3, 2]);
     expect(game.board[3][2]).toBe(ship)
 })
 
 it('attack hit the ship', () => {
-    const game = new Gameboard();
-	game.createBoard();
 	const ship = new Ship('Submarine', 3);
     game.placeShip(ship, [3, 2]);
     game.receiveAttack([3, 2]);
@@ -36,8 +31,6 @@ it('attack hit the ship', () => {
 })
 
 it('report if all ship sunk', () => {
-    const game = new Gameboard();
-	game.createBoard();
 	const ship = new Ship('Submarine', 3);
     game.placeShip(ship, [3, 2]);
     const ship2 = new Ship('Patrol Boat', 2);
