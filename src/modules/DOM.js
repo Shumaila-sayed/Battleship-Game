e448f40d3d28ea9648bfa2070c676a008675d348
Shumaila <sayedshumaila27@gmail.com>
2025-01-31 02:33:38 +0530

    Implement ship display for human

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -1,7 +1,6 @@
-// import { Game } from "./controller";
+import { Game } from "./controller.js";
 
 export const Display = (() => {
-    const container = document.querySelector('.container');
     const playerBoard = document.querySelector('.player-board');
     const computerBoard = document.querySelector('#computer');
     const dialog = document.getElementById('dialog');
@@ -12,12 +11,29 @@ export const Display = (() => {
         
     // }
 
+    function showShips() {
+        const shipPoints = Game.humanPlayer.gameboard.shipArray;
+
+        shipPoints.forEach((points) => {
+            const x = points[0];
+            const y = points[1];
+            const cells = document.querySelectorAll('.player-cell');
+
+            cells.forEach((cell) => {
+                if (parseInt(cell.dataset.indexX) === x && parseInt(cell.dataset.indexY) === y) {
+                    cell.classList.add('ship')
+                }
+            })
+        })
+    }
+
+
     function init() {
         //  setupEventListeners(); 
         for (let i = 0; i < 10; i++) {
             for (let j = 0; j < 10; j++) {
                 const cell = document.createElement('div');
-                cell.className = 'cell';
+                cell.className = 'player-cell';
                 cell.dataset.indexX = i;
                 cell.dataset.indexY = j;
                 playerBoard.appendChild(cell);
@@ -26,18 +42,18 @@ export const Display = (() => {
          for (let i = 0; i < 10; i++) {
 			for (let j = 0; j < 10; j++) {
 				const cell = document.createElement('div');
-				cell.className = 'cell';
+				cell.className = 'computer-cell';
 				cell.dataset.indexX = i;
 				cell.dataset.indexY = j;
 				computerBoard.appendChild(cell);
 				}
-		}
-       
-    }
+        }
 
-    function displayBoard(board) {
+        Game.computerPlayer.gameboard.randomPlaceShip();
+        Game.humanPlayer.gameboard.randomPlaceShip()
+        
+        showShips()
        
-
     }
 
     return { init }
