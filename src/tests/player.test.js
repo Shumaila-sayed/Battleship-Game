0e61eccbdff6247057fb198826afae238104778d
Shumaila <sayedshumaila27@gmail.com>
2025-02-01 02:41:05 +0530

    Fix tests

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -4,14 +4,14 @@ import  Gameboard  from '../modules/Gameboard.js';
 test('toggleTurn method should change isTurn boolean', () => {
 	let player = new Player('Jimmy');
 	player.toggleTurn();
-	expect(player.isTurn).toBeTruthy();
-	player.toggleTurn();
 	expect(player.isTurn).toBeFalsy();
+	player.toggleTurn();
+	expect(player.isTurn).toBeTruthy();
 });
 
 test('ComputerPlayer should have same properties as Player', () => {
 	let cpuPlayer = new ComputerPlayer('Jimmy');
 	expect(cpuPlayer.name).toBe('Jimmy');
 	expect(cpuPlayer.gameboard).toEqual(new Gameboard());
-	expect(cpuPlayer.isTurn).toBeFalsy();
+	expect(cpuPlayer.isTurn).toBeTruthy();
 });
